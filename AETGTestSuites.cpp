1330dcca5bd0bb7b78a586de6ae37691eae61bed
mber1991 <mber1991@hotmail.com>
2014-01-20 17:09:14 -0600

    Initial

---
+++ AETGTestSuites.cpp
@@ -0,0 +1,14 @@
+// AETGTestSuites.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+using namespace std;
+
+
+void main(int argc, _TCHAR* argv[])
+{
+	//fetchInput();
+	//
+}
+
