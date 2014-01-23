659acb39d77b4dc248e1e9ffd30534a607fe192b
mber1991 <mber1991@hotmail.com>
2014-01-23 15:44:50 -0600

    Restructured Functions
    Moved a lot of functions around, optimized some arrays to pointers.

--- AETGTestSuites.cpp
+++ AETGTestSuites.cpp
@@ -1,10 +1,11 @@
 /* AETGTestSuites.cpp : Defines the entry point for the console application.
-	*This code will fetch the Factors and Levels, it will then create 50 candidate test suites and select the best fit.
+	*This code will fetch the Factors and Levels
+	*It will generate a single test suite, each test case will be one of 50 candidates and the best will be selected.
 	*It will run 100 times for each input received and report the average size and execution time
 	*Mixed input is not implemented, every factor will have the same number of levels
 	*Steps:
 		*Fetch Input for Factors and Levels
-		*Generate 50 test suites and pick the ideal (smallest) one
+		*Generate 50 test case candidates and pick the ideal one for each case then generate the suite
 		*Run that 100 times
 */
 
@@ -13,9 +14,13 @@
 using namespace std;
 
 void fetchInput(int &factor, int &level);
-int* generateTestSuite(int factor, int level);
+void generateAllTouples(int factor,int level,int** touplesTable);
+int** generateTestSuite(int factor, int level,int** touplesTable);
 int* generateBestSuite(int factor,int level);
-void generateIndexesTable(int factor,int level,int** indexes);
+
+//Test Suite Array sizes until I optimize it.
+const int ARRAY_SIZE = 20;
+
 /*
 	* Fetch the number of factors and levels and update integers by reference
 	* Visually: A factor is a column, a level is a row
@@ -33,29 +38,59 @@ void fetchInput(int &factor, int &level)
 	*It will create a 2 dimensional array, passed by reference, that holds our indexes
 	*Ex: Factor 0 will have 0, 1, 2 Factor 2 will have 3, 4, 5 etc...
 	*so array[0] will be {0,1,2}
+	*
+	*Then it will create a 2 Dimensional array of every possible Touple and return it
+	*With size of (N X 2)
 */
-void generateIndexesTable(int factor,int level,int** indexes)
+void generateAllTouples(int factor,int level,int** touplesTable)
 {
+	//Create a 2 Dimensional Pointer array that will hold indexes for each Level in a factor
+	int** indexesTable = new int*[factor];
+	for (int s=0; s<factor; s++)
+		indexesTable[s] = new int[level];
+
 	for (int i=0; i<factor; i++)
 	{
 		for (int s=0; s<level; s++)
 		{
-			indexes[i][s] = s+(i*3);
-			cout << indexes[i][s] << " ";
+			indexesTable[i][s] = s+(i*3);
 		}
-		cout << endl;
 	}
-
+	
+	//TODO: I need the EXACT number of possible pairs given a factor and a level
+	//Now we will generate all possible 2-way pairings and assign them to the touplesTable array
+	for (int i=0; i<factor; i++) //Step through each factor once
+	{
+		//Number of pairs left to cover for EACH level in a factor column:
+		//(level*(factor-1)) - (i*level)
+		for (int s=0; s<((level*(factor-1)) - (i*level)); s++) //Step through each level multiple times
+		{
+			
+		}
+	}
+	delete [] indexesTable;
 }
+
 /*
 	* Given Factor and level this generates One Test Suite
 	* And returns a pointer to it
 */
-int* generateTestSuite(int factor, int level)
+int** generateTestSuite(int factor, int level, int** touplesTable)
 {
-	int testSuite[20][20];
-	
-	return *testSuite;
+	int RNGValue;
+	//Initialize a two dimensional pointer array to hold our suite
+	int** testSuite = new int*[ARRAY_SIZE];
+	for (int s=0; s < 20; s++)
+		testSuite[s] = new int[ARRAY_SIZE];
+
+	//Single Test Case:
+		//1. Pick a Factor at random
+		//2. Pick another random Factor, choose a level that will cover the most pairs and break a tie with random decision
+		//3. Continue until you have selcted all factors
+	//If the picking of every factor's level generates no new covered pairs you are done.
+	RNGValue = rand() % 10 + 1;
+
+	return testSuite;
 }
 
 /*
@@ -66,22 +101,23 @@ int* generateBestSuite(int factor,int level)
 {
 	//Maximum suite will be assumed to be 20 x 20 for now
 	//TODO: Optimize array sizes
-	int* allSuites[50]; //Array of pointers to the 50 Suites we generate
-	int bestSuite[20][20]; //The Best Suite we have that will be returned
 	
-	//Create a 2 Dimensional Pointer array that will hold indexes for each Level in a factor
-	int** indexesTable = new int*[factor];
-	for (int s=0; s<factor; s++)
-		indexesTable[s] = new int[level];
-	
-	generateIndexesTable(factor, level, indexesTable);
+	int** bestSuite = new int*[ARRAY_SIZE]; //The Best Suite we have that will be returned
+	for (int s=0; s<ARRAY_SIZE; s++)
+		bestSuite[s] = new int[ARRAY_SIZE];
 
-	for (int i=0; i < 50; i++)
-	{
-		allSuites[i] = generateTestSuite(factor, level);
-	}
+	//Create a 2 Dimensional Pointer array that will hold our touples
+	int toupleArraySize = 50;
+	int coverageValue = 2; //Two-way coverage for now
+	int** touplesTable = new int*[toupleArraySize];
+	for (int s=0; s<toupleArraySize; s++)
+		touplesTable[s] = new int[coverageValue];
 
-	delete [] indexesTable;
+	generateAllTouples(factor, level, touplesTable);
+
+	bestSuite = generateTestSuite(factor, level, touplesTable);
+
+	delete [] touplesTable;
 	return *bestSuite;
 }
 
@@ -91,13 +127,15 @@ void main(int argc, _TCHAR* argv[])
 	int factor,level,dummy;
 	fetchInput(factor,level);
 
+	srand(time(NULL)); //Seed our RNG
+	cout << rand();
+
 	//for (int i=0; i < 100; i++) //We will run our script 100 times to get statistics
 	//{
 		bestSuite = generateBestSuite(factor, level);
-
 		//Math things to calculate time taken and average size
 	//}
-
+	delete [] bestSuite;
 	cin >> dummy; //Pause the script so I can read output
 }
 
